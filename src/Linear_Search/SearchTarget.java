0cc56cd0e9e99b0b950d3edc5c636b9685c1e53d
B-Shankar <bhimashankarholkundi@gmail.com>
2025-01-12 19:05:21 +0530

    Linear search

---
+++ src/Linear_Search/SearchTarget.java
@@ -0,0 +1,38 @@
+package Linear_Search;
+/*
+* Linear Search: Find an element in an array; Value or Index
+* */
+public class SearchTarget {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int target = 4;
+        System.out.println(searchAndReturnIndex(arr, target));
+        System.out.println(searchAndReturnElement(arr, target));
+    }
+
+    public static int searchAndReturnIndex(int[] arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int searchAndReturnElement(int[] arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+        for(int i : arr){
+            if(i == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
