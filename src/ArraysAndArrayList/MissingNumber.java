25bc7ad853b0cc90e8e46a370f16ce0ab2600c4a
B-Shankar <learn.bhimashankar@gmail.com>
2025-12-12 23:26:56 +0530

    Missing number via XOR approach

--- src/ArraysAndArrayList/MissingNumber.java
+++ src/ArraysAndArrayList/MissingNumber.java
@@ -3,6 +3,7 @@ package ArraysAndArrayList;
 // 268 — Missing Number
 public class MissingNumber {
 
+    // Math Formula
     public int missingNumber(int[] nums) {
         int n = nums.length;
         int expected = n * (n + 1) / 2;
@@ -15,10 +16,26 @@ public class MissingNumber {
         return expected - actual;
     }
 
+    // XOR Solution
+    public int missingNumberUsingXOR(int[] nums) {
+        int xor = 0;
+        int n = nums.length;
+
+        for (int i = 0; i <= n; i++) {
+            xor ^= i;
+        }
+        for (int num : nums) {
+            xor ^= num;
+        }
+
+        return xor;
+    }
+
     public static void main(String[] args) {
         MissingNumber obj = new MissingNumber();
         int[] nums = {3, 0, 1};
         int missing = obj.missingNumber(nums);
-        System.out.println("Missing Number: " + missing);
+        int missingXOR = obj.missingNumberUsingXOR(nums);
+        System.out.println("Missing Number: " + missing + " | " + missingXOR);
     }
 }
