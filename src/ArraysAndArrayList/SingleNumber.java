f6419e72ffc3044bae160d4753cf4a88760db22e
B-Shankar <learn.bhimashankar@gmail.com>
2025-12-13 22:29:31 +0530

    Single Number

---
+++ src/ArraysAndArrayList/SingleNumber.java
@@ -0,0 +1,19 @@
+package ArraysAndArrayList;
+
+//136 — Single Number
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SingleNumber obj = new SingleNumber();
+        int[] nums = {4, 1, 2, 1, 2};
+        int single = obj.singleNumber(nums);
+        System.out.println("Single Number: " + single);
+    }
+}
