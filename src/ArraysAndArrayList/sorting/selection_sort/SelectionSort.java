841c9c921a8bab57d7b86de820a11ec87f324c8a
B-Shankar <learn.bhimashankar@gmail.com>
2025-12-16 21:16:26 +0530

    Selection sort

---
+++ src/ArraysAndArrayList/sorting/selection_sort/SelectionSort.java
@@ -0,0 +1,33 @@
+package ArraysAndArrayList.sorting.selection_sort;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // find minimum in unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap min with first element of unsorted part
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
