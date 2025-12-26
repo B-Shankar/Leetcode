1d6fee8fe7e122b16522d22254fe9a4191ec1e73
B-Shankar <learn.bhimashankar@gmail.com>
2025-12-26 19:13:51 +0530

    283: Move Zeroes

---
+++ src/ArraysAndArrayList/MoveZeroes.java
@@ -0,0 +1,29 @@
+package ArraysAndArrayList;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+
+        // Move non-zero elements forward
+        for (int num : nums) {
+            if (num != 0) {
+                nums[pos++] = num;
+            }
+        }
+
+        // Fill remaining with zeroes
+        while (pos < nums.length) {
+            nums[pos++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes mover = new MoveZeroes();
+        int[] arr = {0, 1, 0, 3, 12};
+        mover.moveZeroes(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
