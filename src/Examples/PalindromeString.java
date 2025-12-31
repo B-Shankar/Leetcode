158ffa92db01fc574d9c6bb45c106157cca8f154
B-Shankar <learn.bhimashankar@gmail.com>
2025-12-31 20:37:38 +0530

    PalindromeNumber

--- src/Examples/PalindromeString.java
+++ src/Examples/PalindromeString.java
@@ -18,13 +18,29 @@ public class PalindromeString {
         return true;
     }
 
+    static boolean PalindromeNumber(int num) {
+
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        return (original == reverse);
+    }
+
 
     public static void main(String[] args) {
         System.out.print("Enter String: ");
         Scanner input = new Scanner(System.in);
-        String ip = input.nextLine();
+//        String ip = input.nextLine();
+        int ip = input.nextInt();
 
-        boolean res = isPalindrome(ip);
+//        boolean res = isPalindrome(ip);
+        boolean res = PalindromeNumber(ip);
         if (res) {
             System.out.println(ip + "- is a palindrome string.");
         } else {
