575cb5400a82f23c1fac4d089fba5c930277f3fb
B-Shankar <learn.bhimashankar@gmail.com>
2026-01-01 22:29:26 +0530

    Palindrome non case sensitive

--- src/Examples/PalindromeString.java
+++ src/Examples/PalindromeString.java
@@ -32,15 +32,44 @@ public class PalindromeString {
         return (original == reverse);
     }
 
+    // ip: "A man, a plan, a canal: Panama"
+    static boolean isPalindromeNonCaseSensitive(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // skip non-alphanumeric from left
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            // skip non-alphanumeric from right
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) !=
+                    Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         System.out.print("Enter String: ");
         Scanner input = new Scanner(System.in);
 //        String ip = input.nextLine();
-        int ip = input.nextInt();
+//        int ip = input.nextInt();
+        String ip = input.nextLine();
 
 //        boolean res = isPalindrome(ip);
-        boolean res = PalindromeNumber(ip);
+//        boolean res = PalindromeNumber(ip);
+        boolean res = isPalindromeNonCaseSensitive(ip);
         if (res) {
             System.out.println(ip + "- is a palindrome string.");
         } else {
